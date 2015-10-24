6117de59d739fb0fc057f499ed99180174861bc3
Chris McCormick <chris@mccormick.cx>
2015-10-24 19:25:54 +0800

    Got sprite positioning & wraparound working properly.

--- src/cljs/fubbles/core.cljs
+++ src/cljs/fubbles/core.cljs
@@ -7,8 +7,11 @@
               [cljs-uuid-utils.core :as uuid]
               [infinitelives.utils.console :refer [log]] 
               [infinitelives.utils.events :as evt]
-              [cljs.core.async :refer [chan <! timeout]])
-    (:require-macros [cljs.core.async.macros :refer [go go-loop]])
+              [infinitelives.utils.preload :as preload]
+              [infinitelives.utils.file :refer [resources-to-urls]]
+              [cljs.core.async :refer [chan <! >! put! close! timeout]])
+    (:require-macros [cljs.core.async.macros :refer [go go-loop]]
+                     [infinitelives.utils.file :refer [get-file-list]])
     (:import goog.History))
 
 ; ***** pre-init section ***** ;
@@ -19,6 +22,10 @@
 (defonce game-state (atom {:entities {}}))
 (defonce viewport-size (atom {}))
 (defonce players (atom {}))
+(defonce sprites (atom {}))
+
+; all images from disk
+(def sprite-images (resources-to-urls (get-file-list "resources/public/img/sprites" "png")))
 
 ; ***** functions section ***** ;
 
@@ -32,7 +39,7 @@
 ; get the current time
 (defn get-time-now [] (.getTime (js/Date.)))
 
-; make sprite URL
+; make a sprite URL string
 (defn sprite-url [s] (str "img/sprites/" (name s) ".png"))
 
 ; remove all entities of a particular type (useful for livecoding)
@@ -41,22 +48,28 @@
   (swap! game-state assoc-in [:entities] (into {} (for [[id e] (@game-state :entities)] (if (not (= (e :type) t)) [id e])))))
 
 ; turn a position into a CSS "style" declaration
-(defn compute-position-style [{[x y] :pos s :scale f :flip}]
-  (print "re-computing position style")
+(defn compute-position-style [{[x y] :pos s :scale f :flip v :visibility i :img}]
   (let [w (.-width @viewport-size)
         h (.-height @viewport-size)
-        si (/ 1024.0 (min w h))]
-    {:top (mod (+ (* y h) 
-              (/ h 2.0)) h)
-     :left (mod (+ (* x w)
-               (/ w 2.0)) w)
-     :transform (str "scale(" (* s si) ") scaleX(" f ")")}))
+        sm (min w h)
+        si (/ sm 1024.0)
+        iw (* (.-width (get @sprites i)) s)
+        ih (* (.-height (get @sprites i)) s)]
+    ; (print (.-width (get @sprites i)))
+    {:left (- (mod (+ (* x sm)
+               (/ w 2.0)) (+ w iw)) iw)
+     :top (- (mod (+ (* y sm)
+              (/ h 2.0)) (+ h ih)) ih)
+     :display (case v :visible "block" :invisible "none")
+     :width (str (Math.round iw) "px")
+     :height (str (Math.round ih) "px")
+     :transform (str "scaleX(" f ")")}))
 
 ; insert a single new entity record into the game state and kick off its control loop
-; entity-definition = :symbol :color :pos
+; entity-definition = :img :pos :type :scale :flip etc.
 (defn make-entity [entity-definition]
   (let [id (uuid/uuid-string (uuid/make-random-uuid))
-        entity {id (assoc entity-definition :id id :chan (chan))}]
+        entity {id (assoc entity-definition :id id :chan (chan) :visibility :visible)}]
     ; swap the new entity definition into our game state
     (swap! game-state assoc-in
       [:entities id] (entity id))
@@ -88,20 +101,32 @@
                                 true (old-state :flip))))
 
 ; calculation for which sprite frame to show
-(defn calculate-image-frame-url [now index]
-    (let [frame (+ (mod (js/Math.round (/ now (+ 900 (* index 300)))) 2) 1)
-          img-url (sprite-url (str "c-" (+ index 1) "-" frame))]
-      img-url))
+(defn calculate-player-image-frame-name [now index]
+  (let [frame (+ (mod (js/Math.round (/ now (+ 900 (* index 300)))) 2) 1)
+        img-name (str "c-" (+ index 1) "-" frame)]
+    img-name))
 
 ; choose which frame to show
 (defn update-animation-frame [old-state now gamepad-index !axes]
-    (assoc-in old-state [:img] (calculate-image-frame-url now gamepad-index)))
+  (assoc-in old-state [:img] (calculate-player-image-frame-name now gamepad-index)))
+
+; set the visibility flag
+(defn update-visibility [old-state visibility]
+  (assoc-in old-state [:visibility] visibility))
 
 ; update all of the player state depending on the gamepad input
 (defn update-player [gamepad-object old-state elapsed now]
-  (let [!axes (.-axes gamepad-object)
-        gamepad-index (.-index gamepad-object)]
-    (-> old-state (update-position elapsed !axes 0) (update-position elapsed !axes 1) (update-animation-frame now gamepad-index !axes) (update-sprite-direction !axes))))
+  (if-let [gamepad @gamepad-object]
+    (let [!axes (.-axes gamepad)
+          gamepad-index (.-index gamepad)]
+      (-> old-state
+          (update-position elapsed !axes 0)
+          (update-position elapsed !axes 1)
+          (update-visibility :visible)
+          (update-animation-frame now gamepad-index !axes)
+          (update-sprite-direction !axes)))
+    ; set the player to invisible if their gamepad is disconnected
+    (update-visibility old-state :invisible)))
 
 ; function to create a behaviour function that controls the entity with a gamepad
 (defn make-gamepad-behaviour-fn [gamepad-object]
@@ -109,14 +134,16 @@
     (update-player gamepad-object old-state elapsed now)))
 
 ; create a single player tied to a gamepad object
-(defn make-player [gamepad-object]
-  (let [gamepad-index (.-index gamepad-object)]
-    (if (not (@players gamepad-index))
-      (do
-        (log "Making player with gamepad:" gamepad-index)
-        (let [player (make-entity {:img (calculate-image-frame-url 0 gamepad-index) :pos [0 0] :scale 0.2 :flip 1 :behaviour (make-gamepad-behaviour-fn gamepad-object)})]
-          (swap! players assoc-in [gamepad-index] player)
-          player)))))
+(defn make-player [gamepad-index gamepad-object]
+  (log "Gamepad callback" gamepad-index @gamepad-object)
+  (if @gamepad-object
+    (let [gamepad-index (.-index @gamepad-object)]
+      (if (not (@players gamepad-index))
+        (do
+          (log "Making player with gamepad:" gamepad-index)
+          (let [player (make-entity {:type :player :img (calculate-player-image-frame-name 0 gamepad-index) :pos [0 0] :scale 0.2 :flip 1 :behaviour (make-gamepad-behaviour-fn gamepad-object) :gamepad-object gamepad-object})]
+            (swap! players assoc-in [gamepad-index] player)
+            player))))))
 
 ; behaviour that a cloud follows
 (defn cloud-behaviour [old-state elapsed now]
@@ -127,11 +154,11 @@
 ; select one of the cloud sprites randomly
 (defn choose-cloud []
   (let [cloud-num (js/Math.round (+ (* 11 (rnd)) 1))]
-    (sprite-url (str "cloud-" cloud-num))))
+    (str "cloud-" cloud-num)))
 
 ; create a single cloud object
 (defn make-cloud []
-  (make-entity {:type :cloud :img (choose-cloud) :pos [(- (rnd) 0.5) (- (rnd) 0.5)] :velocity [(* (- (rnd) 0.5) 0.2) (* (- (rnd) 0.5) 0.01)] :scale 0.4 :flip 1 :behaviour cloud-behaviour}))
+  (make-entity {:type :cloud :img (choose-cloud) :pos [(* (- (rnd) 0.5) 2) (* (- (rnd) 0.5) 2)] :velocity [(* (- (rnd) 0.5) 0.2) (* (- (rnd) 0.5) 0.01)] :scale 0.4 :flip 1 :behaviour cloud-behaviour}))
 
 ; ***** renderer ***** ;
 
@@ -140,10 +167,7 @@
     [:div {:id "game-board"}
       ; DOM "scene grapher"
       (doall (map (fn [[id e]]
-                    [:img {:class "sprite"
-                           :key id
-                           :style (compute-position-style e)
-                           :src (e :img)}])
+                    [:img.sprite {:src (sprite-url (e :img)) :key id :style (compute-position-style e)}]) 
                   (:entities @game-state)))]])
 
 ; ***** launch ***** ;
@@ -158,13 +182,14 @@
 
 ; add some cloud objects
 (remove-type :cloud)
-(doseq [c (range 5)] (make-cloud))
-
-; all images from disk
-(def sprite-images (resources-to-urls (get-file-list "resources/public/img/sprites" "png")))
+(doseq [c (range 15)] (make-cloud))
 
 ; get a handle on our progress bar
 (def progress-bar (.getElementById js/document "progress-bar"))
+
+(defn mount-root []
+  (print "Mounting react root.")
+  (reagent/render [dom-base] (.getElementById js/document "app")))
 
 ; pre-load all of our images
 (defn preload-sprites []
@@ -177,6 +202,10 @@
                   (print "There was an error loading one of the resources")
                   (do
                     (<! (timeout (* (rnd) 1000)))
+                    (let [basename (.replace (last (.split (.-src img) "/")) ".png" "")]
+                      ; (print basename)
+                      (swap! sprites assoc basename img)
+                      (print sprites))
                     (set! progress-bar.style.width (str (Math.round (* (- 1.0 (/ remaining num-urls)) 100.0)) "%"))
                     (recur)))))))
         (print "Finished loading images.")
